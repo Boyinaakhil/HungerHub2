0c59d4ee93303b9ca1550f651f9a819ae67459d1
Boyina Akhil <151818306+Boyinaakhil@users.noreply.github.com>
2026-04-14 00:35:30 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
+        origin: "https://hungerhub-05l1.onrender.com",
         credentials: true,
         methods: ['POST', 'GET']
     }
@@ -32,7 +32,7 @@ app.set("io", io)
 const port = process.env.PORT || 8000 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
+    origin: "https://hungerhub-05l1.onrender.com",
     credentials: true
 }))
 app.use(express.json())
